fe9682f781a78c2fc40145d6263ed587ea66cfa0
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-01-07 16:23:23 +0100

    Basic CRUD functionality

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-let age: number;
+import express, {Request, Response, NextFunction } from 'express'
+import { json } from 'body-parser'
+import todoRoutes from './routes/todos'
 
-age = 30;
 
-console.log(age);
+const app = express()
+
+app.use(json())
+
+app.use('/todos', todoRoutes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message})
+})
+
+app.listen(3000)
